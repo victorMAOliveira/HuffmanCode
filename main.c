d7ba245c70277c687d56fbcabeb06ca08a5c28ae
Victor Oliveira <victor.moura.amaral.oliveira@gmail.com>
2025-10-09 17:31:15 -0300

    1.0

--- main.c
+++ main.c
@@ -4,6 +4,8 @@
 #include <string.h>
 
 #define FREQ_SIZE 256
+#define MAX_FILE_NAME_SIZE 1000
+#define TREE_BUFFER_MAX_SIZE 10000
 
 typedef struct Node
 {
@@ -15,14 +17,10 @@ typedef struct List
     void *head, *size;
 } List;
 
-void get_freq(unsigned char *content, unsigned int freq[])
+void get_freq(unsigned char *content, unsigned int freq[], size_t size)
 {
-    unsigned int i = 0;
-    while (content[i] != '\0')
-    {
+    for (size_t i = 0; i < size; i++)
         freq[content[i]]++;
-        i++;
-    }
 }
 
 void print_freq(unsigned int freq[])
@@ -80,7 +78,6 @@ void fill_list(unsigned int freq[], List *list)
                 new->next = NULL;
                 new->left = NULL;
                 new->right = NULL;
-
                 insert_sorted(list, new);
             }
             else
@@ -143,30 +140,24 @@ Node *build_tree(List *list)
     return list->head;
 }
 
-void print_tree(Node *root, unsigned int size)
+void free_tree(Node *root)
 {
-    if (!root->left && !root->right)
-        printf("\tfolha: %c\taltura: %u\n", *(unsigned char *)root->c, size);
-    else
-    {
-        print_tree(root->left, size + 1);
-        print_tree(root->right, size + 1);
-    }
+    if (!root)
+        return;
+    free_tree(root->left);
+    free_tree(root->right);
+    free(root->c);
+    free(root->freq);
+    free(root);
 }
 
 int tree_height(Node *root)
 {
     if (!root)
         return -1;
-    else
-    {
-        int left = tree_height(root->left) + 1;
-        int right = tree_height(root->right) + 1;
-        if (left > right)
-            return left;
-        else
-            return right;
-    }
+    int left = tree_height(root->left) + 1;
+    int right = tree_height(root->right) + 1;
+    return (left > right) ? left : right;
 }
 
 char **allocate_table(int columns)
@@ -193,57 +184,86 @@ void build_table(char **table, Node *root, char *path, int columns)
     }
 }
 
-void print_table(char **table)
+int size_str(char **table, unsigned char *content, size_t size)
 {
-    for (int i = 0; i < FREQ_SIZE; i++)
-    {
-        if (strlen(table[i]) > 0)
-            printf("\t%3d: %s\n", i, table[i]);
-    }
+    int total = 0;
+    for (size_t i = 0; i < size; i++)
+        total += strlen(table[content[i]]);
+    return total + 1;
 }
 
-int size_str(char **table, unsigned char *content)
+char *encode(char **table, unsigned char *content, size_t size)
 {
-    int i = 0, size = 0;
-    while (content[i] != '\0')
-        size += strlen(table[content[i++]]);
-    return size + 1;
-}
-
-char *encode(char **table, unsigned char *content)
-{
-    char *codigo = calloc(size_str(table, content), sizeof(char));
-    int i = 0;
-    while (content[i] != '\0')
-        strcat(codigo, table[content[i++]]);
+    char *codigo = calloc(size_str(table, content, size), sizeof(char));
+    for (size_t i = 0; i < size; i++)
+        strcat(codigo, table[content[i]]);
     return codigo;
 }
 
-char *decode(unsigned char content[], Node *root)
+unsigned int is_bit_one(unsigned char byte, int i)
 {
-    int i = 0;
-    Node *aux = root;
-    char *decoded = calloc(strlen(content), sizeof(char));
-    char temp[2];
-    while (content[i] != '\0')
-    {   
-        if (content[i] == '0')
-            aux = aux->left;
-        else
-            aux = aux->right;
-        if (!aux->left && !aux->right)
+    unsigned char mask = 1 << i;
+    return byte & mask;
+}
+
+void build_tree_buffer(Node *root, char *buffer, int *index)
+{
+    if (!root->left && !root->right)
+    {
+        buffer[(*index)++] = '1';
+        unsigned char c = *(unsigned char *)root->c;
+        for (int i = 7; i >= 0; i--)
+            buffer[(*index)++] = (c & (1 << i)) ? '1' : '0';
+    }
+    else
+    {
+        buffer[(*index)++] = '0';
+        build_tree_buffer(root->left, buffer, index);
+        build_tree_buffer(root->right, buffer, index);
+    }
+}
+
+void write_bit_str(FILE *file, char *bit_str, int bit_qnt)
+{
+    int i = 0, j = 7;
+    unsigned char byte = 0;
+    while (i < bit_qnt)
+    {
+        if (bit_str[i] == '1')
+            byte |= (1 << j);
+        j--;
+        if (j < 0)
         {
-            temp[0] = *(unsigned char *)aux->c;
-            temp[1] = '\0';
-            strcat(decoded, temp);
-            aux = root;
+            fwrite(&byte, sizeof(unsigned char), 1, file);
+            byte = 0;
+            j = 7;
         }
         i++;
     }
-    return decoded;
+    if (j != 7)
+        fwrite(&byte, sizeof(unsigned char), 1, file);
 }
 
-void compress(unsigned char str[], char file_name[])
+void mount_header(FILE *file, unsigned char str[], Node *root)
+{
+    unsigned int trash_size = strlen((char *)str) % 8;
+
+    char tree_bits[TREE_BUFFER_MAX_SIZE];
+    int index = 0;
+    build_tree_buffer(root, tree_bits, &index);
+
+    int bit_qnt = index;
+    unsigned int tree_size = (bit_qnt + 7) / 8;
+
+    unsigned char byte1 = (trash_size << 5) | (tree_size >> 8);
+    unsigned char byte2 = tree_size & 255;
+
+    fwrite(&byte1, sizeof(unsigned char), 1, file);
+    fwrite(&byte2, sizeof(unsigned char), 1, file);
+    write_bit_str(file, tree_bits, bit_qnt);
+}
+
+void compress(unsigned char str[], char file_name[], Node *root)
 {
     FILE *huff_file = fopen(file_name, "wb");
     if (!huff_file)
@@ -251,16 +271,13 @@ void compress(unsigned char str[], char file_name[])
         printf("erro ao abrir arquivo em compress()\n");
         return;
     }
+    mount_header(huff_file, str, root);
     int i = 0, j = 7;
-    unsigned char byte = 0, mask;
+    unsigned char byte = 0;
     while (str[i] != '\0')
     {
-        mask = 1;
         if (str[i] == '1')
-        {
-            mask = mask << j;
-            byte = byte | mask;
-        }
+            byte |= (1 << j);
         j--;
         if (j < 0)
         {
@@ -270,44 +287,194 @@ void compress(unsigned char str[], char file_name[])
         }
         i++;
     }
+    if (j != 7)
+        fwrite(&byte, sizeof(unsigned char), 1, huff_file);
+    fclose(huff_file);
+}
+
+Node *build_tree_from_bits(unsigned char *buffer, int *index, int total_bits)
+{
+    if (*index >= total_bits)
+        return NULL;
+    if (buffer[*index] == '1')
+    {
+        (*index)++;
+        unsigned char c = 0;
+        for (int i = 0; i < 8 && *index < total_bits; i++, (*index)++)
+        {
+            c <<= 1;
+            if (buffer[*index] == '1')
+                c |= 1;
+        }
+        Node *leaf = malloc(sizeof(Node));
+        leaf->c = malloc(sizeof(unsigned char));
+        *(unsigned char *)leaf->c = c;
+        leaf->freq = malloc(sizeof(unsigned int));
+        *(unsigned int *)leaf->freq = 0;
+        leaf->left = leaf->right = leaf->next = NULL;
+        return leaf;
+    }
+    else if (buffer[*index] == '0')
+    {
+        (*index)++;
+        Node *node = malloc(sizeof(Node));
+        node->c = malloc(sizeof(unsigned char));
+        *(unsigned char *)node->c = '*';
+        node->freq = malloc(sizeof(unsigned int));
+        *(unsigned int *)node->freq = 0;
+        node->next = NULL;
+        node->left = build_tree_from_bits(buffer, index, total_bits);
+        node->right = build_tree_from_bits(buffer, index, total_bits);
+        return node;
+    }
+    return NULL;
+}
+
+Node *build_tree_from_header(FILE *file)
+{
+    unsigned char byte1, byte2;
+    fread(&byte1, sizeof(unsigned char), 1, file);
+    fread(&byte2, sizeof(unsigned char), 1, file);
+
+    unsigned int trash_size = byte1 >> 5;
+    unsigned int tree_size = ((byte1 & 31) << 8) | byte2;
+
+    unsigned char *tree_bytes = malloc(tree_size);
+    fread(tree_bytes, sizeof(unsigned char), tree_size, file);
+
+    unsigned char *tree_bits = malloc(tree_size * 8 + 1);
+    int index = 0;
+    for (unsigned int i = 0; i < tree_size; i++)
+        for (int j = 7; j >= 0; j--)
+            tree_bits[index++] = (is_bit_one(tree_bytes[i], j)) ? '1' : '0';
+    tree_bits[index] = '\0';
+
+    int bit_index = 0;
+    Node *root = build_tree_from_bits(tree_bits, &bit_index, index);
+
+    free(tree_bytes);
+    free(tree_bits);
+    (void)trash_size;
+    return root;
+}
+
+char *file_without_dot_huff(char *file_name)
+{
+    char *new_name = malloc(MAX_FILE_NAME_SIZE);
+    strcpy(new_name, file_name);
+    new_name[strcspn(new_name, ".")] = '\0';
+    return new_name;
+}
+
+void extract(char huff_name[])
+{
+    FILE *file = fopen(huff_name, "rb");
+    if (!file)
+    {
+        printf("erro ao abrir arquivo em extract()\n");
+        return;
+    }
+
+    char *file_without_huff = file_without_dot_huff(huff_name);
+    FILE *extracted = fopen(file_without_huff, "wb");
+    Node *root = build_tree_from_header(file);
+
+    unsigned char byte = 0;
+    Node *aux = root;
+    while (fread(&byte, sizeof(unsigned char), 1, file))
+    {
+        for (int i = 7; i >= 0; i--)
+        {
+            aux = is_bit_one(byte, i) ? aux->right : aux->left;
+            if (!aux->left && !aux->right)
+            {
+                fwrite((char *)aux->c, sizeof(char), 1, extracted);
+                aux = root;
+            }
+        }
+    }
+
+    free_tree(root);
+    free(file_without_huff);
+    fclose(extracted);
+    fclose(file);
 }
 
 int main()
 {
-    setlocale(LC_ALL, "Portuguese");
+    setlocale(LC_ALL, "C");
 
-    unsigned char content[] = "Tio Marcio e fera";
+    int opcao;
+    char file_name[MAX_FILE_NAME_SIZE];
 
-    unsigned int freq[FREQ_SIZE];
-    for (int i = 0; i < FREQ_SIZE; i++)
+    printf("=== COMPRESSOR HUFFMAN ===\n");
+    printf("1 - Comprimir arquivo\n");
+    printf("2 - Extrair arquivo (.huff)\n");
+    printf("Escolha uma opcao: ");
+    scanf("%d", &opcao);
+    getchar();
+
+    if (opcao == 1)
     {
-        freq[i] = 0;
+        printf("Caminho do arquivo a ser comprimido: ");
+        scanf("%s", file_name);
+
+        FILE *file = fopen(file_name, "rb");
+        if (!file)
+        {
+            printf("Erro ao abrir arquivo para leitura.\n");
+            return 1;
+        }
+
+        fseek(file, 0, SEEK_END);
+        long file_size = ftell(file);
+        fseek(file, 0, SEEK_SET);
+
+        unsigned char *content = calloc(file_size, sizeof(unsigned char));
+        fread(content, sizeof(unsigned char), file_size, file);
+        fclose(file);
+
+        unsigned int freq[FREQ_SIZE] = {0};
+        get_freq(content, freq, file_size);
+
+        List list;
+        create_list(&list);
+        fill_list(freq, &list);
+
+        Node *root = build_tree(&list);
+
+        int columns = tree_height(root) + 2;
+        char **table = allocate_table(columns);
+        char path[columns];
+        strcpy(path, "");
+        build_table(table, root, path, columns);
+
+        char *encoded = encode(table, content, file_size);
+
+        char compressed_name[MAX_FILE_NAME_SIZE];
+        snprintf(compressed_name, sizeof(compressed_name), "%s.huff", file_name);
+
+        compress((unsigned char *)encoded, compressed_name, root);
+        printf("Arquivo comprimido com sucesso: %s\n", compressed_name);
+
+        free_tree(root);
+        for (int i = 0; i < FREQ_SIZE; i++)
+            free(table[i]);
+        free(table);
+        free(content);
+        free(encoded);
     }
-    get_freq(content, freq);
-    printf("\nfrequencias:\n");
-    print_freq(freq);
-
-    List list;
-    create_list(&list);
-    fill_list(freq, &list);
-    printf("\nlista:\n");
-    print_list(&list);
-
-    Node *tree = build_tree(&list);
-    printf("\narvore:\n");
-    print_tree(tree, 0);
-
-    int columns = tree_height(tree) + 1;
-    char **table = allocate_table(columns);
-    build_table(table, tree, "", columns);
-    printf("\ntabela:\n");
-    print_table(table);
-
-    char *encoded = encode(table, content);
-    printf("\n\tcodificado: %s\n", encoded);
-
-    char *decoded = decode(encoded, tree);
-    printf("\n\tdecodificado: %s\n", decoded);
+    else if (opcao == 2)
+    {
+        printf("Caminho do arquivo .huff a ser extraido: ");
+        scanf("%s", file_name);
+        extract(file_name);
+        printf("Arquivo extraido com sucesso.\n");
+    }
+    else
+    {
+        printf("Opcao invalida.\n");
+    }
 
     return 0;
 }
